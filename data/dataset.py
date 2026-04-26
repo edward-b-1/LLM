abf263edc84446d51b453402cbf6d213edce6cda
Edward <example@example.com>
2026-04-26 14:41:17 +0100

    Add SQuAD dataset

--- data/dataset.py
+++ data/dataset.py
@@ -4,6 +4,40 @@ import torch
 from torch.utils.data import Dataset
 
 
+class SquadDataset(Dataset):
+    def __init__(self, split, context_length):
+        from data.prepare import DATASET_DIR
+        tokens_path = os.path.join(DATASET_DIR, f"squad_{split}_tokens.npy")
+        masks_path  = os.path.join(DATASET_DIR, f"squad_{split}_masks.npy")
+        self.examples      = np.load(tokens_path, allow_pickle=True)
+        self.masks         = np.load(masks_path,  allow_pickle=True)
+        self.context_length = context_length
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, idx):
+        tokens = self.examples[idx]
+        mask   = self.masks[idx]
+
+        # Truncate to context_length + 1 (need one extra for the shift)
+        tokens = tokens[:self.context_length + 1]
+        mask   = mask[:self.context_length + 1]
+
+        # Pad to context_length + 1
+        pad_len = self.context_length + 1 - len(tokens)
+        tokens  = np.array(tokens, dtype=np.int64)
+        mask    = np.array(mask,   dtype=np.float32)
+        if pad_len > 0:
+            tokens = np.concatenate([tokens, np.zeros(pad_len, dtype=np.int64)])
+            mask   = np.concatenate([mask,   np.zeros(pad_len, dtype=np.float32)])
+
+        x         = torch.from_numpy(tokens[:-1])
+        y         = torch.from_numpy(tokens[1:])
+        loss_mask = torch.from_numpy(mask[1:])   # aligned with targets
+        return x, y, loss_mask
+
+
 class TokenDataset(Dataset):
     def __init__(self, path, context_length):
         self.tokens = np.memmap(path, dtype=np.uint16, mode='r')
